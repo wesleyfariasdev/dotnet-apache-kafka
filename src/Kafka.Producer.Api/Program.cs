42e18f284b6d15a9ac389597d9b7da01841547d8
Wesley Farias <Wesley Farias@DESKTOP-CITMI5B>
2025-08-24 01:01:09 -0300

    injeta dependencia e mapeia rota usando minimal api

--- src/Kafka.Producer.Api/Program.cs
+++ src/Kafka.Producer.Api/Program.cs
@@ -1,6 +1,14 @@
+using Kafka.Producer.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<ProducerServices>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/", async ([FromServices] ProducerServices services, [FromQuery] string message) =>
+{
+    return await services.SendMessage(message);
+});
 
 app.Run();

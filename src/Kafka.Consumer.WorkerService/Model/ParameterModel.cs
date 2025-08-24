d464a5b1e8413596ed2b1035da84f98920f2b55a
Wesley Farias <Wesley Farias@DESKTOP-CITMI5B>
2025-08-24 02:35:30 -0300

    feat: parameter model para facilitar a leitura dos logs

---
+++ src/Kafka.Consumer.WorkerService/Model/ParameterModel.cs
@@ -0,0 +1,10 @@
+﻿using Confluent.Kafka;
+
+namespace Kafka.Consumer.WorkerService.Model;
+
+internal sealed class ParameterModel(IConfiguration _configuration)
+{
+    public string BootstrapServers { get; set; } = _configuration["KafkaConfig:BootstrapServer"]!;
+    public string TopicName { get; set; } = _configuration["KafkaConfig:TopicName"]!;
+    public string GroupId { get; set; } = _configuration["KafkaConfig:GroupId"]!;
+}

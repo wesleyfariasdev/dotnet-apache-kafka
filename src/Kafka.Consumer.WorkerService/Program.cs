a57ccf87077e8266626687583082a929d499c7cb
Wesley Farias <Wesley Farias@DESKTOP-CITMI5B>
2025-08-24 02:35:01 -0300

    injeta a dependencia das configuração do IConsumer

--- src/Kafka.Consumer.WorkerService/Program.cs
+++ src/Kafka.Consumer.WorkerService/Program.cs
@@ -1,6 +1,20 @@
+using Confluent.Kafka;
 using Kafka.Consumer.WorkerService;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSingleton<IConsumer<Ignore, string>>(sp =>
+{
+    var configuration = sp.GetRequiredService<IConfiguration>();
+    var consumerConfig = new ConsumerConfig
+    {
+        BootstrapServers = configuration["KafkaConfig:BootstrapServer"],
+        GroupId = configuration["KafkaConfig:GroupId"],
+        AutoOffsetReset = AutoOffsetReset.Earliest
+    };
+    return new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+});
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();

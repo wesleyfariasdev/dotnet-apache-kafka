0931056fa5c205ad80496e222f528c4d9dc28dea
unknown <wesleyfarias.dev@gmail.com>
2026-05-30 18:29:30 -0300

    add web project

---
+++ src/Biblioteca.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Biblioteca.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
